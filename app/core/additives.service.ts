3ccbdf7235947658751ecb82275fe9a6da4252c1
sevdalinapashova91 <sevdalina.pashova@yahoo.com>
2016-12-29 18:20:05 +0200

    //detailed fixed with json file

--- app/core/additives.service.ts
+++ app/core/additives.service.ts
@@ -1,27 +1,38 @@
 import { Injectable }     from '@angular/core';
-import { Http, Response, URLSearchParams } from '@angular/http';
+import { Http, Response, URLSearchParams, Headers, RequestOptions  } from '@angular/http';
 import { FoodAdditive } from './additive.model';
 import { Observable }     from 'rxjs/Observable';
 
 @Injectable()
 export class AdditiveService {
-  private heroesUrl = '../data/foodAdditivies.json';
+  private additivesUrl = '../data/foodAdditivies.json';
   constructor (private http: Http) {}
   getAdditives (): Observable<FoodAdditive[]> {
-    return this.http.get(this.heroesUrl)
+    return this.http.get(this.additivesUrl)
                     .map(this.extractData)
                     .catch(this.handleError);
   }
-  getAdditiveById(id: string) {
-    let params: URLSearchParams = new URLSearchParams();
-    params.set('Id', id);
-    return this.http.get(this.heroesUrl,{
-          search : params
-       }).map(this.extractData)
+  getAdditiveById(id: string) : Promise<FoodAdditive>{   
+    return this.http.get(this.additivesUrl)
+      .map(this.extractData)
+      .toPromise()    
+      .then(ad=>ad.find(a=>a.Id == id))
       .catch(this.handleError);
   }
-  private extractData(res: Response) {
+  
+  addAdditive (id: string, name : string, image : string, category: string, quantity : string, ingredients: string, purpose: string, madeBy: string):
+   Observable<FoodAdditive> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(this.additivesUrl, { id, name, image, category, quantity, ingredients,purpose, madeBy }, options)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private extractData(res: Response) : FoodAdditive[]{
     let body = res.json();
+    body = body as FoodAdditive[];
     console.log(body);
     return body || { };
   }
