981a25e61469b2e8057ae76be803f4f2e3dcb096
sevdalinapashova91 <sevdalina.pashova@yahoo.com>
2017-01-02 14:10:03 +0200

    // favourite button and navigation bar added, favorite functionality not yet implemented

--- app/additive/additive-detail.component.ts
+++ app/additive/additive-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { FoodAdditive } from '../core/models/additive.model';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { AdditiveService }  from '../core/services/additives.service';
+import { AuthenticationService } from '../core/services/authentication.service';
 
 @Component({
     selector: 'mvdb-additive-detailed',
@@ -10,12 +11,20 @@ import { AdditiveService }  from '../core/services/additives.service';
 
 export class AdditiveDetailComponent{
   additive: FoodAdditive;
+ 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private service: AdditiveService
+    private service: AdditiveService,
+    private authService: AuthenticationService
   ) {}
 
+  public isLogged() {
+      return this.authService.checkIfUserIsLoggedIn();
+   }
+  saveToFavorites(){
+
+  }
   ngOnInit() {
     this.route.params
       .switchMap((params: Params) => this.service.getAdditiveById(params['id']))
