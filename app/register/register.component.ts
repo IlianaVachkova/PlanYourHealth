7dc7f5e2ba506009d1bec802a13c6d2b6acd535b
IlianaVachkova <an_vachkova@yahoo.com>
2017-01-02 04:00:24 +0200

    Added client side validation for register form.

--- app/register/register.component.ts
+++ app/register/register.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+
 import { UserService } from '../core/services/user.service';
 
 @Component({
 	templateUrl: './register.component.html'
 })
 export class RegisterComponent {
-	model: any = {};
-	loading: boolean = false;
+	private model: any = {};
+	private loading: boolean = false;
+	private successfullRegistrationMessage: string = 'Your registration is successful!';
+	private errorRegistrationMessage: string = 'Incorrect input!';
+    private minUsernameLength: number = 3;
+    private maxUsernameLength: number = 30;
+    private minPasswordLength: number = 5;
+    private maxPasswordLength: number = 30;
 
 	constructor(
 		private router: Router,
-		private userService: UserService
+		private userService: UserService,
+		public toastr: ToastsManager
 	) { }
 
 	register() {
@@ -20,10 +29,20 @@ export class RegisterComponent {
 		this.userService.create(this.model)
 			.subscribe(
 			data => {
+				this.showSuccessfulRegistrationMessage();
 				this.router.navigate(['user/login']);
 			},
 			error => {
+				this.showErrorRegistrationMessage();
 				this.loading = false;
 			});
+	}
+
+	showSuccessfulRegistrationMessage(){
+		this.toastr.success(this.successfullRegistrationMessage);
+	}
+
+	showErrorRegistrationMessage(){
+		this.toastr.error(this.errorRegistrationMessage);
 	}
 }
