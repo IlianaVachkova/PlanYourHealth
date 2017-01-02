af62671a23b46f07d4043ff2344f2522c3e4c2e9
sevdalinapashova91 <sevdalina.pashova@yahoo.com>
2017-01-02 22:54:28 +0200

    // home page with carrossel

--- app/home.component.ts
+++ app/home.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'home',
-    template: '<h1>Здравейте! Това е най-готиния сайт за хранителни добавки! Разгледайте ни</h1>'
+    templateUrl: './home.component.html'
 })
 
 export class HomeComponent{
