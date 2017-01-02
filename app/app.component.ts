cad824dca2cff8b7b3f30c916dd6f2c63bb0f243
IlianaVachkova <an_vachkova@yahoo.com>
2017-01-02 04:52:27 +0200

    Added toastr messages to login and register.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
+
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 @Component({
     selector: 'mvdb-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
 })
-export class AppComponent {}
+
+export class AppComponent {
+    constructor(private toastr: ToastsManager, private viewContainerRef: ViewContainerRef) {
+		this.toastr.setRootViewContainerRef(viewContainerRef);
+	}
+}
